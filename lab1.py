a1484948c6953220986000aedafe3829082427ca
ecekrn <86051693+ecekrn@users.noreply.github.com>
2022-03-30 21:39:51 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,18 @@
+#QUESTION 1
+''' name = input('What is your name?')
+print(name)
+print(f'Hello {name}')
+
+id= input(f'Hello {name}, What is your student id?')
+print(id)
+print(f'Your student id is {id}') '''
+
+#QUESTION 2
+num1 = input('Enter the first value: ')
+num2 = input('Enter the second value: ')
+
+sum = int(num1) + int (num2)
+diff = int(num1) + int(num2)
+prod = int(num1) + int(num2)
+
+print('Sum:' +str(sum))
